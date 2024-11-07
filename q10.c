b4364170bc027fc2fcb5959b73ca51f3ca3c2a97
Conspline007 <thamaraikamal07@gmail.com>
2024-11-07 06:27:13 +0530

    update

--- q10.c
+++ q10.c
@@ -2,5 +2,21 @@
 
 int main()
 {
+    int n,i,j,k=0,f;
+    printf("Enter the length of the array : "); scanf ("%d",&n);
     
+    int arr[n], freq[1000000];
+    for (i=0;i<n;i++)
+    {
+        f=0;
+        for (j=0;j<n;j++)
+        {
+            if (arr[i]==arr[j])   
+            ;         
+        }
+    }
+
+    
+    
+    return 0;
 }
